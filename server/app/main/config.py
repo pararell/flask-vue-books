ec04f7d546a63416fe98c9c41a933f84c597c61f
Miroslav Smrtic <miroslav.smrtic@pelikan.sk>
2019-09-30 10:38:09 +0200

    Add CLEARDB MySQL DB on Heroku

--- server/app/main/config.py
+++ server/app/main/config.py
@@ -3,6 +3,6 @@ import os
 class BaseConfig(object):
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     DEBUG = os.environ.get('DEBUG')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     GOODREADS_KEY = os.environ.get('GOODREADS_KEY')
